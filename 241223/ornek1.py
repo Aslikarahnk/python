f4e677744777eb75ccc14f18c1986aaff2095b8b
Aslikarahnk <aslikarahnk@gmail.com>
2024-12-23 11:47:14 +0300

    Add files via upload

---
+++ 241223/ornek1.py
@@ -0,0 +1,4 @@
+birim_fiyat=20
+metrekup_miktar=int(input("metrekup miktarını yazınız:"))
+tutar(birim_fiyat*metrekup_miktar)
+print(tutar)
---
+++ 241223/ornek2.py
@@ -0,0 +1,6 @@
+not1=int(input("birirnci notu giriniz:"))
+not2=int(input("ikinci notu giriniz:"))
+performans1=int(input("birinci performans notunu giriniz:"))
+performans2=int(input("ikinci performans notunu giriniz:"))
+ortalama=(not1+not2+performans1+performans2)/4
+print("4 notun ortalaması:",ortalama)
