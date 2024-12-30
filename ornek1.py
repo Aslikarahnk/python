e4edde2f0deb0d08bb601d04173df21686ef793f
Aslikarahnk <aslikarahnk@gmail.com>
2024-12-30 11:29:47 +0300

    Add files via upload

---
+++ ornek1.py
@@ -0,0 +1,4 @@
+fiyat=15
+simit=int(input("ödenecek tutar:"))
+toplam=(fiyat*simit)
+print("simitlerin toplamı:",toplam)
---
+++ ornek2.py
@@ -0,0 +1,5 @@
+hiz=int(input("aldığı km:"))
+süre=int(input("gidilen süre:"))
+mesafe=int(input("gitmesi gereken mesafe:"))
+kalan_mesafe=(hiz*süre)-mesafe
+print("kalan_mesafe:",kalan_mesafe)
